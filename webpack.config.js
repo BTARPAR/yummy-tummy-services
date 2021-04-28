ea3aa83ab41762523593d64a98abcc7bab5ac525
Bhargav Tarpara <bhargav_tarpara@yahoo.co.in>
2021-04-27 19:10:38 -0700

    UPDATE WEBPACK

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 const webpackConfig = {
-    entry: './checkAuth.js',
+    entry: './index.js',
     target: 'node',
     plugins: [
         new CleanWebpackPlugin()
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'checkAuth.js'
+        filename: 'index.js'
     },
     context: path.resolve(__dirname, '.'),
     module: {
