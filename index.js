d910ba504e0bc5a8c7af1752f7cb09cdfe7ee8ff
Bhargav Tarpara <bhargav_tarpara@yahoo.co.in>
2021-04-29 16:38:48 -0700

    DEPLOY

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json())
 // }
 //INCOMING DOMAIN LIST HEAR
 app.use(cors({ origin: 'http://localhost:8080' , credentials :  true}))
-// app.use(cors({ origin: 'https://y-tummy.netlify.app/' , credentials :  true}))
+app.use(cors({ origin: 'https://y-tummy.netlify.app/' , credentials :  true}))
 // app.options(cors());
 
 //=======================
